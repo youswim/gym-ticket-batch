b6bb6398ba8fe1182583a4ee972c3c54b0de494c
youswim <youswim96@gmail.com>
2023-07-02 14:02:18 +0900

    init: project init

---
+++ src/main/java/com/sy/gymticketbatch/GymTicketBatchApplication.java
@@ -0,0 +1,13 @@
+package com.sy.gymticketbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTicketBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymTicketBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sy/gymticketbatch/GymTicketBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.gymticketbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTicketBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
